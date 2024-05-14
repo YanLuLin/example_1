1090fb4996df6eaf0190e4537605487a0e0bbdcc
YanLuLin <163007489+YanLuLin@users.noreply.github.com>
2024-05-15 00:01:14 +0800

    Update SOR_eg.m

--- SOR_eg.m
+++ SOR_eg.m
@@ -1,10 +1,11 @@
 function [opt_omega,opt_IT,opt_cputime,res]=SOR_eg(m,maxit,omega_0,omega_N,delta_omega)
+n=m^2;
 tol=1.0e-6;
 [A,b,xk] = eg_1(m);
 N_omega=floor((omega_N-omega_0)/delta_omega);        
 for j=1:N_omega
     omega(j)=omega_0+j*delta_omega;
-    [IT(j),cputime(j),res(j)]=SOR(A,b,xk,omega(j),tol,maxit);
+    [IT(j),cputime(j),res(j)]=SOR(n,A,b,xk,omega(j),tol,maxit);
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 plot(omega,IT)
