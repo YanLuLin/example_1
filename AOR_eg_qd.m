3d79cf870a67af0c153ca5ecc91ae69b00e288a7
YanLuLin <163007489+YanLuLin@users.noreply.github.com>
2024-05-14 23:54:56 +0800

    Update AOR_eg_qd.m

--- AOR_eg_qd.m
+++ AOR_eg_qd.m
@@ -1,4 +1,5 @@
 function [omega,gamma,IT,cputime,res]=AOR_eg_qd(m,maxit,omega,gamma)
+n=m^2;
 tol=1.0e-6;
 [A,b,xk] = eg_1(m);
-[IT,cputime,res,omega,gamma]=AOR(A,b,xk,omega,gamma,tol,maxit); 
+[IT,cputime,res,omega,gamma]=AOR(n,A,b,xk,omega,gamma,tol,maxit); 
