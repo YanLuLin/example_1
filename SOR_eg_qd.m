05172c7403895f9b4cdf49eeeb61683ac5b2aca6
YanLuLin <163007489+YanLuLin@users.noreply.github.com>
2024-05-15 00:01:53 +0800

    Update SOR_eg_qd.m

--- SOR_eg_qd.m
+++ SOR_eg_qd.m
@@ -1,4 +1,5 @@
 function [omega,IT,CPU,RES]=SOR_eg_qd(m,maxit,omega)
+n=m^2;
 eta=1.0e-6;
 [A,b,xk] = eg_1(m);
-[IT,CPU,RES]=SOR(A,b,xk,omega,eta,maxit);
+[IT,CPU,RES]=SOR(n,A,b,xk,omega,eta,maxit);
